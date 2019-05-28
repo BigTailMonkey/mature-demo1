c6e4e09ad1eb6223195cc7782433d4e25c1f7263
BigTailMonkey <hdw_worker@163.com>
2019-05-28 10:55:05 +0800

    新增公共模块，用于存放公共POJO文件。 但此种方式会造成各模块之间的高耦合，需全面衡量后决定。 此处为示例方便，使用此种方式。

---
+++ common-modul/src/main/java/com/btm/commonmodul/modul/UserModul.java
@@ -0,0 +1,16 @@
+package com.btm.commonmodul.modul;
+
+import lombok.Data;
+
+/**
+ * @Author: BigTailMonkey
+ * @Date: 2019/5/28 10:09
+ * @Version: 1.0
+ */
+@Data
+public class UserModul {
+
+    private String name;
+    private String sex;
+    private int age;
+}
